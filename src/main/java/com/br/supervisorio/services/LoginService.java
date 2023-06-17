6ed0e293d621ebb2261e9721965c774499906fbb
Murilo Vieira Cruz <murilo12super@gmail.com>
2023-06-17 15:23:05 -0300

    Exceptions
    Added implementation of custom exceptions

--- src/main/java/com/br/supervisorio/services/LoginService.java
+++ src/main/java/com/br/supervisorio/services/LoginService.java
@@ -2,6 +2,7 @@ package com.br.supervisorio.services;
 
 import com.br.supervisorio.entities.Login;
 import com.br.supervisorio.repositories.LoginRepository;
+import com.br.supervisorio.services.exceptions.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -12,6 +13,7 @@ public class LoginService {
     private final LoginRepository loginRepository;
 
     public Login findById(Long id) {
-        return loginRepository.findById(id).get();
+        return loginRepository.findById(id).orElseThrow(
+                () -> new EntityNotFoundException("The id " + id + " was not found!"));
     }
 }
